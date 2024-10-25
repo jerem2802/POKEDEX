185ba17d8d621f1340dc2c4f19c47dbfde820da4
jerem2802 <creamedoc@gmail.com>
2024-10-25 10:52:02 +0200

    modif

---
+++ src/Components/PokemonCard.tsx
@@ -0,0 +1,31 @@
+function PokemonCard() {
+	const pokemon = pokemonList[1];
+	return (
+		<figure>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
+}
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+
+	{
+		name: "mew",
+	},
+
+	{
+		name: "mewtoo",
+	},
+];
+export default PokemonCard;
